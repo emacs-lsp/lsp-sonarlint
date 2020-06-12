bf07ae5f93cfbcbcbf181ff4d794d491e4690f35
sasanidas <fmfs@posteo.net>
2020-06-12 05:20:22 +0200

    Delete css, it seems like is not compatible
    Former-commit-id: 74d4af996298df67c676f43f6253dac141c78ac3 [formerly ee6dfa28827e9548e45402f939bdf5acdce3018f]
    Former-commit-id: e0b7c97647e32a08b5cf797616a068a4f2d01d97

--- lsp-sonarlint.el
+++ lsp-sonarlint.el
@@ -148,24 +148,6 @@
   :type 'file)
 
 
-
-(defgroup lsp-sonarlint-css nil
-  "lsp-sonarlint css analyzer group"
-  :group 'lsp-sonarlint)
-
-(defcustom lsp-sonarlint-css-enabled t
-  "Lps-sonarlint css analyzer option."
-  :group 'lsp-sonarlint-css
-  :type 'boolean)
-
-(defcustom lsp-sonarlint-css-analyzer-path
-  "/mnt/Documentos/Descargas/sonar-css.jar"
-  "Lsp-sonarlint css analyzer location."
-  :group 'lsp-sonarlint-css
-  :type 'file)
-
-
-
 (defun lsp-sonarlint-server-start-fun (port)
   "Lsp-sonarlint start function, it need PORT as parameter."
   (-non-nil
@@ -183,10 +165,7 @@
 	 (concat "file://" lsp-sonarlint-javascript-analyzer-path " "))
 
     ,(when lsp-sonarlint-java-enabled
-       (concat "file://" lsp-sonarlint-java-analyzer-path " " ))
-
-    ,(when lsp-sonarlint-css-enabled
-       (concat "file://" lsp-sonarlint-css-analyzer-path )))))
+       (concat "file://" lsp-sonarlint-java-analyzer-path )))))
 
 
 (lsp-register-custom-settings
