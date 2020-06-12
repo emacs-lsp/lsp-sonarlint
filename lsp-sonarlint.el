aa2f484a50cfd556d6525e3cfd31921bd02acd64
sasanidas <fmfs@posteo.net>
2020-06-12 02:24:55 +0200

    Update link
    Former-commit-id: f8108c413faf08ed46b0587196337b2e2e65f1c5 [formerly 203c3535bcced5dfd88966b10752c9c0f42f6c04]
    Former-commit-id: 56018b8ef4d2501a4e8993f948defd4960aeac10

--- lsp-sonarlint.el
+++ lsp-sonarlint.el
@@ -38,7 +38,7 @@
 (defgroup lsp-sonarlint nil
   "Sonarlint lsp server group"
   :group 'lsp-mode
-  :link '(url-link "")
+  :link '(url-link "https://gitlab.com/sasanidas/lsp-sonarlint")
   :package-version '(lsp-mode . "6.4"))
 
 (defcustom lsp-sonarlint-server-path
