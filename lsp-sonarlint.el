8f9555cd76df7bb01d51a920a84183b3d2c61e74
sasanidas <fmfs@posteo.net>
2020-06-13 04:48:22 +0200

    Add scala default mode

--- lsp-sonarlint.el
+++ lsp-sonarlint.el
@@ -50,7 +50,7 @@
  :group 'lsp-sonarlint
  :type 'file)
 
-(defcustom lsp-sonarlint-modes-enabled '(php-mode html-mode web-mode js-mode js2-mode python-mode java-mode css-mode ruby-mode)
+(defcustom lsp-sonarlint-modes-enabled '(php-mode html-mode web-mode js-mode js2-mode python-mode java-mode css-mode ruby-mode scala-mode)
   "Lsp-sonarlint activation modes."
   :group 'lsp-sonarlint
   :type 'file)
