2a9fdf63c067fc1dbe8675f2eb5f6b7e76f46036
sasanidas <fmfs@posteo.net>
2020-06-14 13:52:40 +0200

    Fix errata

--- lsp-sonarlint.el
+++ lsp-sonarlint.el
@@ -27,7 +27,7 @@
 ;; Sonarlint LSP extensions for GNU Emacs, add support for the majority of sonarlint languages
 ;; including: php, javascript, typescript, html, python and java.
 
-;; This is NOT and official Sonarlint extension.
+;; This is NOT an official Sonarlint extension.
 
 
 ;;; Code:
